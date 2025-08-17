87f206e81cf09f7146b7c048ace8c6910be5bbfb
kainshel <shelllllman@gmail.com>
2025-08-18 00:09:49 +0300

    Update app_exercises.js

--- app_exercises.js
+++ app_exercises.js
@@ -126,7 +126,7 @@
     const gr = (filterGroups?.value || '').trim();
     if (gr) list = list.filter(ex => ex.groups.includes(gr));
 
-    const tg = (filterTargetS?.value || '').trim();
+    const tg = (filterTargets?.value || '').trim();
     if (tg) list = list.filter(ex => ex.targets.includes(tg));
 
     const t = (filterType?.value || '').trim();
